64bf4d98b6445f3fa8f203ab4428e489b1f8b040
Tri Nguyen <mail@trimn.net>
2022-11-22 22:08:00 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,43 @@
+const EARTH_RADIUS_KILOMETER: f64 = 6371.0;
+
+fn round(val: f64) -> f64 {
+    (100.0 * val).round() / 100.0
+}
+
+pub struct Location {
+    lat: f64,
+    lon: f64,
+}
+
+pub fn haversine(start: &Location, end: &Location) -> f64 {
+    let φ1: f64 = start.lat.to_radians();
+    let φ2: f64 = end.lat.to_radians();
+    let δφ: f64 = (end.lat - start.lat).to_radians();
+    let δλ: f64 = (end.lon - start.lon).to_radians();
+
+    let a: f64 = (δφ / 2.0).sin() * (δφ / 2.0).sin()
+        + φ1.cos() * φ2.cos() * (δλ / 2.0).sin() * (δλ / 2.0).sin();
+    let c: f64 = 2.0 * (a.sqrt()).asin();
+
+    round(EARTH_RADIUS_KILOMETER * c)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_calculates_correnct_distance_in_km() {
+        let start: Location = Location {
+            lat: 59.890465,
+            lon: 10.523493,
+        };
+
+        let end: Location = Location {
+            lat: 59.904499,
+            lon: 10.786372,
+        };
+
+        assert_eq!(14.74, haversine(&start, &end));
+    }
+}
